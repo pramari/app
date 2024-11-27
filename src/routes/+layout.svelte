9ff869c30e9c7c058749186c7dd70d0a2de51a30
Andreas <andreas@neumeier.org>
2024-11-27 09:05:41 +0000

    layout.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>import "../app.css";</script><slot></slot>
 
+<script>
+ let { children } = $props();
+</script>
+
 <nav>
   <a href="/">Home</a>
   <a href="/about">About</a>
 </nav>
+
+{@render children()}
