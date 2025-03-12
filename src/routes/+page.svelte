89d51d669b6fdd5e757f1ef42c7bc0e5e3557466
Andreas <andreas.neumeier@sap.com>
2025-03-12 10:06:50 +0100

    tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>
     Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
     documentation
 </p>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-
 <style lang="postcss">
-    @reference "tailwindcss";
+    @reference "tailwindcss/base";
     :global(html) {
-        background-color: theme(--color-gray-100);
+        /*    background-color: #cccccc;  theme(--color-gray-100);*/
     }
 </style>
