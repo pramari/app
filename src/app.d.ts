5e8da9f40820392d2f2c94cf698f94a2f41ad7c6
Andreas <andreas@neumeier.org>
2025-09-16 18:08:55 +0200

    Update app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -22,8 +22,8 @@ declare global {
 // Additional environment variables type definitions
 declare namespace NodeJS {
   interface ProcessEnv {
-    GOOGLE_CLIENT_ID: string;
-    GOOGLE_CLIENT_SECRET: string;
+    PRAMARI_CLIENT_ID: string;
+    PRAMARI_CLIENT_SECRET: string;
     AUTH_SECRET: string;
   }
 }
