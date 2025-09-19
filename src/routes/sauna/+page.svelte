11405409e6d4de3f42115c1150b26d014ea8dfb3
Andreas <andreas@neumeier.org>
2025-09-19 13:59:02 +0000

    Update +page.svelte

--- src/routes/sauna/+page.svelte
+++ src/routes/sauna/+page.svelte
@@ -32,13 +32,13 @@
 
 		const redirectUrl = getBaseRedirectUrl();
 		logToUI(`Redirect URL: ${redirectUrl}`); // Log redirect URL for debugging
-		alert('Redirecting for SSO login... ' + redirectUrl);
 		const ssoRedirectEndpoint =
 			homeserverUrl.replace(/\/$/, '') +
 			'/_matrix/client/v3/login/sso/redirect?redirectUrl=' +
 			encodeURIComponent(redirectUrl);
 
 		logToUI(`SSO Redirect Endpoint: ${ssoRedirectEndpoint}`); // Log SSO endpoint
+		alert('Redirecting for SSO login... ' + ssoRedirectEndpoint);
 		window.location.href = ssoRedirectEndpoint;
 	}
 
